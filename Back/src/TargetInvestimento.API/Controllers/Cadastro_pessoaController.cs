c488a119b5ce38ec7ddc1a05ad9c64422f278178
Fael414 <rafaelcorreagsilva@gmail.com>
2022-03-01 11:44:34 -0300

    V 0.1

---
+++ Back/src/TargetInvestimento.API/Controllers/Cadastro_pessoaController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TargetInvestimento.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class Cadastro_pessoaController : ControllerBase
+    {
+        public IEnumerable<Cadastro_pessoa> _Cadastro_pessoa = new Cadastro_pessoa[]{
+                new Cadastro_pessoa(){
+                    Cadastro_pessoaID= 1,
+                    NomeCompleto = "Jose da Silva",
+                    DataNascimento = "21/05/1990",
+                    CPF = "11122233344" ,
+                    Logradouro = "Rua dos bobos",
+                    Bairro = "Taquara",
+                    CEP= "12345678",
+                    Cidade = "Rio de Janeiro",
+                    UF = "RJ",
+                    Complemento = "casa",
+                    Renda_mensal = 5000,
+                },
+                new Cadastro_pessoa(){
+                    Cadastro_pessoaID= 2,
+                    NomeCompleto = "Lucas Mota",
+                    DataNascimento = "08/09/1993",
+                    CPF = "22233344455" ,
+                    Logradouro = "Rua Visconde de Inhauma",
+                    Bairro = "Centro",
+                    CEP= "87654321",
+                    Cidade = "Rio de Janeiro",
+                    UF = "RJ",
+                    Complemento = "Apt 201",
+                    Renda_mensal = 8000,
+                }
+            };
+
+        public Cadastro_pessoaController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Cadastro_pessoa> Get()
+        {
+            return _Cadastro_pessoa;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Cadastro_pessoa> GetById(int id)
+        {
+            return _Cadastro_pessoa.Where(evento => evento.Cadastro_pessoaID == id);
+        }
+        // usando o postman, o professor deu exemplos com metodo Post
+        [HttpPost]
+        public IEnumerable<Cadastro_pessoa> Post()
+        {
+            return _Cadastro_pessoa;
+        }
+
+        [HttpPost("{id}")]
+        public IEnumerable<Cadastro_pessoa> PostById(int id)
+        {
+            return _Cadastro_pessoa.Where(evento => evento.Cadastro_pessoaID == id);
+        }
+
+    }
+}
+
---
+++ Back/src/TargetInvestimento.API/Models/Cadastro_pessoa.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace TargetInvestimento.API
+{
+    public class Cadastro_pessoa
+    {
+        public int Cadastro_pessoaID { get; set; }
+
+        public string NomeCompleto { get; set; }
+
+        public string DataNascimento { get; set; }
+
+        public string CPF { get; set; }
+        public string Logradouro { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Cidade { get; set; }
+    
+        public string UF { get; set; }
+        public string Complemento { get; set; }
+
+        public int Renda_mensal { get; set; }
+
+    }
+}
